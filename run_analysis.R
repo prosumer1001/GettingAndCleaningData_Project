3a537bdb208c33f05469b0fafed6791280ca46ae
prosumer1001 <prosumer1001@gmail.com>
2020-05-17 21:35:48 +0900

    Initial commit

---
+++ run_analysis.R
@@ -0,0 +1,6 @@
+## Coursera Getting and Cleaning Data
+## script name:  run_analysis.R
+## Prepared by Nick Thompson
+## 17 MAY 2020 - 01 JUN 2020
+## See accompanying files:  README.md; CODEBOOK.md
+
