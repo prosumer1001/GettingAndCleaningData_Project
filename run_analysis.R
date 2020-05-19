23ef52707683c72a81be2e23faeeb14f7fb2f52e
prosumer1001 <prosumer1001@gmail.com>
2020-05-19 21:21:53 +0900

    wrote code that transforms the strings of V2 in the features data to variable names.  Next task is noted on lines 32-33: run stringer, tolower, and gsub to remove punctuation and upper case letters.  After that, the features dataset needs to become the variable names for the x_text data.

--- run_analysis.R
+++ run_analysis.R
@@ -14,7 +14,7 @@ unique(subject_test)
 x_text <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/x_test.txt")
 str(x_text)
 unique(x_text)
-View(x_text)
+#View(x_text)
 head(x_text)
 tail(x_text)
 names(x_text)
@@ -25,10 +25,18 @@ str(y_text)
 unique(y_text)
 
 features <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/features.txt")
-str(features)
-unique(features)
-View(features)
-head(features)
-tail(features$V1)
-names(features)
-
+features <- features %>%  # change names of variables and select only the strings
+        rename(objectid = "V1", featureNames = "V2") %>%
+        select(featureNames)
+names(features) # check names
+## TODO on 5/19/2020 I left of needing TODO line 33.
+## In this spot, before converting the vector string to colnames, run tolower, stringer, and gsub to remove all punctuation and upper case letters
+var2 <- c(1:561) # create placeholder container 1
+var3 <- c(1:561) # create placeholder container 2
+features <- cbind(features, var2) # bind container 1 to features
+features <- cbind(features, var3) # bind container 2 to features
+features <- features %>% pivot_wider(names_from = featureNames, values_from = var2) # use dplyr pivot_wider to change the strings to column names
+features <- features %>% select(-var3) # remove the exta empty container that did not pivot wider
+features <- features[0,] # remove all rows from data frame (tibble)
+View(features) # check data frame
+str(features) # check data frame
