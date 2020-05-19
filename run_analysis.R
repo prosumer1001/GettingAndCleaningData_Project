8672e25a88c429410e60a31b02452106d5bab267
prosumer1001 <prosumer1001@gmail.com>
2020-05-19 18:02:05 +0900

    updates run_analysis.R and codebook with slight modifications.  added preliminary datasets to run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -4,5 +4,31 @@
 ## 17 MAY 2020 - 01 JUN 2020
 ## See accompanying files:  README.md; CODEBOOK.md
 
+library(purrr)
+library(tidyverse)
 library(rio)
-data01 <- https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip
+subject_test <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/subject_test.txt")
+str(subject_test)
+unique(subject_test)
+
+x_text <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/x_test.txt")
+str(x_text)
+unique(x_text)
+View(x_text)
+head(x_text)
+tail(x_text)
+names(x_text)
+## The features number of observations is 561, which is the number of variables that the x_text file has.  That means that I need to transpose the features observations into variables.
+
+y_text <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/y_test.txt")
+str(y_text)
+unique(y_text)
+
+features <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/features.txt")
+str(features)
+unique(features)
+View(features)
+head(features)
+tail(features$V1)
+names(features)
+
