d9114fadebc79c664d028c9fce90878c91d2fc0a
prosumer1001 <prosumer1001@gmail.com>
2020-05-20 21:40:08 +0900

    modified code to place all featureNames vector names into the variable/column names of x_text.txt raw data, then saved that as a new data frame called `completeDF.RData` in the Project/Data/Processed.

--- run_analysis.R
+++ run_analysis.R
@@ -7,36 +7,34 @@
 library(purrr)
 library(tidyverse)
 library(rio)
+library(stringr)
 subject_test <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/subject_test.txt")
 str(subject_test)
 unique(subject_test)
 
 x_text <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/x_test.txt")
 str(x_text)
+str(features)
 unique(x_text)
-#View(x_text)
+View(x_text)
 head(x_text)
 tail(x_text)
-names(x_text)
+View(x_text)
 ## The features number of observations is 561, which is the number of variables that the x_text file has.  That means that I need to transpose the features observations into variables.
 
+## TODO: process the y_text file
 y_text <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/y_test.txt")
-str(y_text)
+str(y_text); View(y_text); head(y_text)
 unique(y_text)
 
-features <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/features.txt")
+
+features <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/features.txt") # import features.txt data into working environment
+x_text <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/x_test.txt") # import features.txt data into working environment
+features$V2 <- str_replace_all(features$V2, "[[:punct:]]", "") # remove all punctuation and ensure there are no spaces
+features$V2 <- tolower(features$V2)  # make all alpha characters lowercase
 features <- features %>%  # change names of variables and select only the strings
         rename(objectid = "V1", featureNames = "V2") %>%
         select(featureNames)
-names(features) # check names
-## TODO on 5/19/2020 I left of needing TODO line 33.
-## In this spot, before converting the vector string to colnames, run tolower, stringer, and gsub to remove all punctuation and upper case letters
-var2 <- c(1:561) # create placeholder container 1
-var3 <- c(1:561) # create placeholder container 2
-features <- cbind(features, var2) # bind container 1 to features
-features <- cbind(features, var3) # bind container 2 to features
-features <- features %>% pivot_wider(names_from = featureNames, values_from = var2) # use dplyr pivot_wider to change the strings to column names
-features <- features %>% select(-var3) # remove the exta empty container that did not pivot wider
-features <- features[0,] # remove all rows from data frame (tibble)
-View(features) # check data frame
-str(features) # check data frame
+names(x_text) <- features$featureNames # make the vector featureNames the column names for x_text
+completeDF <- x_text # create new processed dataframe called `completeDF` that will ultimately have all the test data included once processed
+export(completeDF, file = "~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Processed/completeDF.RData") # save processed data to appropriate directory
