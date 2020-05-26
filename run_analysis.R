438c9327f6c427ac6e3b1935930ecd5a31b45876
prosumer1001 <prosumer1001@gmail.com>
2020-05-26 19:40:17 +0900

    created the partialDF and updated the CODEBOOK.Rmd.  TODO is outlined in run_analysis.

--- run_analysis.R
+++ run_analysis.R
@@ -8,33 +8,50 @@ library(purrr)
 library(tidyverse)
 library(rio)
 library(stringr)
-subject_test <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/subject_test.txt")
+subject_test <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/test/subject_test.txt")
 str(subject_test)
 unique(subject_test)
+subject_test
+View(subject_test)
 
-x_text <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/x_test.txt")
-str(x_text)
+x_test <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/test/x_test.txt")
+str(x_test)
 str(features)
-unique(x_text)
-View(x_text)
-head(x_text)
-tail(x_text)
-View(x_text)
-## The features number of observations is 561, which is the number of variables that the x_text file has.  That means that I need to transpose the features observations into variables.
+unique(x_test)
+View(x_test)
+head(x_test)
+tail(x_test)
+View(x_test)
+## TODO:  Below to the cutline is the code that makes the partialDF.  The next thing to do is combine the partialDF and the completeDF.  Then all of the steps need to be outlined in the CODEBOOK.Rmd
+subject_test <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/test/subject_test.txt")
+y_test <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/test/y_test.txt")
+activity_labels <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/activity_labels.txt")
+unique(activity_labels)
+y_test <- tibble::rowid_to_column(y_test, "ID")
+subject_test <- tibble::rowid_to_column(subject_test, "ID")
+partialDF <- left_join(y_test, subject_test, by = "ID")
+partialDF <- partialDF %>% rename(factor = V1.x)
+partialDF <- partialDF %>% rename(subject = V1.y)
+activity_labels <- activity_labels %>% rename(factor = V1)
+activity_labels <- activity_labels %>% rename(activity_category = V2)
+str(list(partialDF, activity_labels))
+example <- sample_n(partialDF, 6)
+partialDF <- left_join(partialDF, activity_labels, by = "factor"); str(partialDF)
+sample_n(partialDF, 25)
+y_test <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/test/y_test.txt")
+str(y_test); View(y_test); head(y_test)
+unique(y_test)
+## CUT LINE ##
 
-## TODO: process the y_text file
-y_text <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/y_test.txt")
-str(y_text); View(y_text); head(y_text)
-unique(y_text)
 
 
 features <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/features.txt") # import features.txt data into working environment
-x_text <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/x_test.txt") # import features.txt data into working environment
+x_test <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/UCIHARDataset/test/x_test.txt") # import features.txt data into working environment
 features$V2 <- str_replace_all(features$V2, "[[:punct:]]", "") # remove all punctuation and ensure there are no spaces
 features$V2 <- tolower(features$V2)  # make all alpha characters lowercase
 features <- features %>%  # change names of variables and select only the strings
         rename(objectid = "V1", featureNames = "V2") %>%
         select(featureNames)
-names(x_text) <- features$featureNames # make the vector featureNames the column names for x_text
-completeDF <- x_text # create new processed dataframe called `completeDF` that will ultimately have all the test data included once processed
+names(x_test) <- features$featureNames # make the vector featureNames the column names for x_test
+completeDF <- x_test # create new processed dataframe called `completeDF` that will ultimately have all the test data included once processed
 export(completeDF, file = "~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Processed/completeDF.RData") # save processed data to appropriate directory
