4284c3745015cac45ca2121abcee6231a6b468b1
prosumer1001 <prosumer1001@gmail.com>
2020-05-30 20:27:46 +0900

    cleaned up the TOC and the steps needed to complete the data frames.  There are now two tidy data frames, one for training and one for testing, that will be merged in the next step.  Once the merging is complete, most likely through rbind(), the DF can be saved, the script saved, the CODEBOOK.Rmd written, and the README.Rmd written.  Then the assignment will be complete.

--- run_analysis.R
+++ run_analysis.R
@@ -4,11 +4,22 @@
 ## 17 MAY 2020 - 01 JUN 2020
 ## See accompanying files:  README.md; CODEBOOK.md
 
-library(tidyverse)
-library(rio)
-library(stringr)
-library(janitor)
-library(arsenal)
+if(T){ # Preamble
+ipak <- function(pkg){
+        new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+        if (length(new.pkg)) 
+                install.packages(new.pkg, dependencies = TRUE)
+        sapply(pkg, require, character.only = TRUE)
+}
+# Install and Load Packages
+packages <- c("arsenal",
+              "janitor",
+              "rio", 
+              "stringr",
+              "tidyverse")
+ipak(packages)
+} # Preamble
+
 ## Table of Contents (TOC)
 ## 1. Import Data
 ##      A. y_test (TEST LABELS)
@@ -31,7 +42,8 @@ library(arsenal)
 ##      B. Create the xtestResults and xtrainResults data frames. 
 
 ## 4. Add front matter to xtrainResults and xtestResults DFs.
-##      A. 
+##      A. Build the xtest results DF with front matter
+##      B. Build the xtrain results DF with front matter
 
 
 ## TOC For DFs Created
@@ -52,11 +64,14 @@ processeddata <- "~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Proje
         ## A. y_test (TEST LABLES)
 y_test <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/test/y_test.txt") ## load the y_test.txt data file
 y_test <- tibble::rowid_to_column(y_test, "ID") ## create a unique variable named `ID` in the y_test for left_join(by = "ID")
+y_test <- y_test %>% rename(factor = "V1"); str(y_test)
+
 
         ## B. y_train (TRAINING LABELS)
 y_train <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/train/y_train.txt")
 y_train <- tibble::rowid_to_column(y_train, "ID") ## create a unique variable named `ID` in the y_train for left_join(by = "ID")
 str(y_train)
+y_train <- y_train %>% rename(factor = "V1"); str(y_train)
 
         ## C. x_test (TEST DATA)
 x_test <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/test/x_test.txt") # import features.txt data into working environment
@@ -76,18 +91,21 @@ names(x_train)
 
         ## E. subject_test (TEST)
 subject_test <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/test/subject_test.txt") ## load the subject_test.txt (the person's ID number that was in the test) data file
-subject_test <- tibble::rowid_to_column(subject_test, "ID") ## create a unique variable named `ID` in the subject_test for left_join(by = "ID")
+subject_test <- tibble::rowid_to_column(subject_test, "ID"); str(subject_test) ## create a unique variable named `ID` in the subject_test for left_join(by = "ID")
+subject_test <- subject_test %>% rename(subjecttestID = "V1"); str(subject_test)
 
         ## F. subject_train
 subject_train <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/train/subject_train.txt") ## load the subject_train.txt data file)
-subject_train <- tibble::rowid_to_column(subject_train, "ID") ## create a unique variable named `ID` in the subject_train for left_join(by = "ID")
+subject_train <- tibble::rowid_to_column(subject_train, "ID"); str(subject_train) ## create a unique variable named `ID` in the subject_train for left_join(by = "ID")
+subject_train <- subject_train %>% rename(subjecttrainID = "V1"); str(subject_train)
 
         ## G. activity_lables
 activity_labels <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/activity_labels.txt") ## load the activity_labels.txt data file
 labelsDF <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/activity_labels.txt") # import labelsDF.RData
 labelsDF <- labelsDF %>% rename(ID = "V1")
-labelsDF <- labelsDF %>% rename(LABELS = "V2")
-labelsDF
+labelsDF <- labelsDF %>% rename(label = "V2"); str(labelsDF)
+labelsDF <- labelsDF %>% mutate(factor = ID); str(labelsDF)
+labelsDF <- labelsDF %>% select(ID, factor, label)
 export(labelsDF, file = "~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Processed/labels.RData")
 labelsDF <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Processed/labels.RData")
 
@@ -95,11 +113,10 @@ labelsDF <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Pro
 features <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Raw/features.txt"); head(features) # import features.txt data into working environment
 ID <- "ID"
 features <- rbind(features, ID); names(features); head(features); tail(features); sample_n(features, 10)
-features %>% filter(V1 >= 461 & V1 <= 502)
 
 } ## END SECTION 1 ##
 
-if(T){ ## BEGIN SECTION 2 ##
+if(F){ ## BEGIN SECTION 2 ## NOT USED
 ## 2. Create FRONT MATTER (Activity and Subject Data Frames) (activityDF v1.0 and subjectDF v1.0)
 ##      A. Create Activity Labels Data Frame for Joining to y_test and y_train
 y_test <- y_test %>% rename(label = V1) ## rename variable for future join
@@ -132,7 +149,7 @@ subject_train <- subject_train %>% rename(trainingsubjectid = V1)
 subjectDF <- full_join(subject_test, subject_train, by = "ID"); sample_n(subjectDF, 5) ## left_join subject_test and subject_train by = "ID" to make subjectDF (version 1.0)
 # sample_n(subjectDF, 25) ## take a sample of the partialDF to make sure that the DF is complete and accurate
 
-} ## END SECTION 2 ##
+} ## END SECTION 2 ## NOT USED
 
 if(T){ ## BEGIN SECTION 3 ##
 ##      A. Create the rows data frame that will select the correct variables
@@ -176,35 +193,61 @@ xtrainResults <- tibble::rowid_to_column(xtrainResults, "ID"); names(xtrainResul
 
 if(T){## BEGIN SECTION 4 ##
 ## 4. Add front matter to xtrainResults and xtestResults DFs.
+##      Code below creates two data frames: xtestResults & xtrainResults WITH front matter.
+##      A. Build the xtest results DF with front matter
+frontMatterTest <- left_join(y_test, subject_test, by = "ID"); str(frontMatterTest); names(frontMatterTest)
+frontMatterTest <- left_join(frontMatterTest, labelsDF, by = "factor"); str(xtestResults);  names(frontMatterTest)
+frontMatterTest <- frontMatterTest %>% rename(ID = ID.x)
+frontMatterTest <- frontMatterTest %>% select(-ID.y)
+xtestResults <- left_join(frontMatterTest, xtestResults, by = "ID"); str(xtestResults) ## left join by the ID variable
 
-## Code below creates two data frames: xtestResults & xtrainResults WITH front matter.
+##      B. Build the xtrain results DF with front matter
+frontMatterTrain <- left_join(y_train, subject_train, by = "ID"); str(frontMatterTrain); names(frontMatterTrain)
+frontMatterTrain <- left_join(frontMatterTrain, labelsDF, by = "factor"); str(xtrainResults);  names(frontMatterTrain)
+frontMatterTrain <- frontMatterTrain %>% rename(ID = ID.x)
+frontMatterTrain <- frontMatterTrain %>% select(-ID.y)
+xtrainResults <- left_join(frontMatterTrain, xtrainResults, by = "ID"); str(xtrainResults) ## left join by the ID variable
 
-## TODO: You have not make this code work yet.  This is old code that takes 
-##              the labelsDF and xtestResults/xtrainResults DFs and combines
-##              them into DFs with front matter.
-completeDF <- import("~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Processed/completeDF.RData") ## import completeDF.RData
-finaltestDF <- left_join(labelsDF, completeDF, by = "ID") ## left join by the ID variable
-finaltestDF <- finaltestDF %>% rename(activityfactor = factor) ## rename the factor variable to be congruent with dataframe naming convention
-finaltestDF <- finaltestDF %>% rename(subjectnumber = subject) ## rename the subject variable to be congruent with dataframe naming convention
-finaltestDF <- finaltestDF %>% rename(objectid = ID) ## rename the ID variable to be the objectid variable
-str(finaltestDF)
-finaltestDF$activityfactor <- as.factor(finaltestDF$activityfactor)
-finaltestDF$activitycategory <- as.factor(finaltestDF$activitycategory)
-finaltestDF <- finaltestDF %>% select(c(objectid, activityfactor, activitycategory, subjectnumber), everything())
-export(finaltestDF, file = "~/Documents/gitrepos/DataAnalysis/GettingAndCleaningData_Project/Project/Data/Processed/finaltestDF.RData") # save processed data to appropriate directory
+View(xtrainResults)
+View(xtestResults)
+
 
 } ## END SECTION 4 ##
 
+if(T){ ## BEGIN SECTION 5 ##
+##      5. Add experimentType column to each DF, "train" for training and
+##              "test" for testing
+##      A. Add column for experimentType for testing.
+experimentType <- data.frame(experimentType = "test", stringsAsFactors = FALSE)
+experimentType <- experimentType %>% slice(rep(1:n(), each=2947)); str(experimentType)
+experimentType <- tibble::rowid_to_column(experimentType, "ID"); str(experimentType)
+xtestResults <- left_join(xtestResults, experimentType); names(xtestResults)
+xtestResults <- xtestResults %>% select(ID, factor, subjecttestID, label, experimentType, everything()); names(xtestResults)
+
+##      A. Add column for experimentType for training
+experimentType <- data.frame(experimentType = "train", stringsAsFactors = FALSE)
+experimentType <- experimentType %>% slice(rep(1:n(), each=7352)); str(experimentType)
+experimentType <- tibble::rowid_to_column(experimentType, "ID"); str(experimentType)
+xtrainResults <- left_join(xtrainResults, experimentType); names(xtrainResults)
+xtrainResults <- xtrainResults %>% select(ID, factor, subjecttrainID, label, experimentType, everything()); names(xtrainResults)
+
+        } ## END SECTION 5 ##
+
 ## TODO:  the data frame that captures the correct training and test 
-##        observations (see .  There will be many variables since both the training 
-##        and the test data have the same variables--each of which has 561 
-##        variables.  This should be done in three primary steps.  
+##        observations (see .  There will be many variables since both the 
+##        training and the test data have the same variables--each of which 
+##        has 561 variables.  This should be done in three primary steps.  
+
 ##        Step 1. make the x_train (-> xtrainResults) and 
 ##                x_test (-> xtestResults), untidy, data frames - COMPLETE 
-##        STEP 2. add front matter to the xtrainResults & xtestResults DFs
-##        Step 2. make the x_train and x_test, tidy, data frames -   
-##        Step 3. merge the x_train and x_test, tidy, data frames into the 
+##        STEP 2. add front matter to the xtrainResults & xtestResults 
+##                DFs - COMPLETE
+##        Step 3. make the xtestResults and xtrainResults, tidy, 
+##                data frames - COMPLETE  
+##        Step 4. merge the x_train and x_test, tidy, data frames into the 
 ##                `completeDF` data frame.
+##        Step 5. write the README.Rmd
+##        Step 6. write the CODEBOOK.Rmd
 
 
 
